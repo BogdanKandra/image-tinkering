64970c6efad536f96ba23eaa4c124e85564b50fe
Bogdan Kandra <bogdan.kandra@gmail.com>
2018-09-14 00:29:56 +0300

    Added function on button click

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ Created on Wed Sep 12 21:34:01 2018
 
 @author: Bogdan
 """
-from tkinter import *
+from tkinter import Frame, Button, Tk
+#from tkinter import *
 
 class Window(Frame):
     
@@ -15,8 +16,11 @@ class Window(Frame):
     def init_window(self):
         self.master.title('HELLO TKINTER')
         self.pack(fill=BOTH, expand=1)
-        quitButton = Button(self, text='Quit')
+        quitButton = Button(self, text='Quit', command=self.app_exit)
         quitButton.place(x=0, y=0)
+        
+    def app_exit(self):
+        self.master.destroy()
         
 mainWindow = Tk()
 mainWindow.geometry('400x300')
