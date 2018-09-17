c73841b817b9a8a7ec15600f2338f737835d0a04
Bogdan Kandra <bogdan.kandra@gmail.com>
2018-09-17 19:01:14 +0300

    Added Menus

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ Created on Wed Sep 12 21:34:01 2018
 
 @author: Bogdan
 """
-from tkinter import Frame, Button, Tk
-#from tkinter import *
+#from tkinter import Frame, Button, Tk
+from tkinter import *
 
 class Window(Frame):
     
@@ -14,15 +14,24 @@ class Window(Frame):
         self.init_window()
         
     def init_window(self):
-        self.master.title('HELLO TKINTER')
+        self.master.title('HELLO TKINTER') # Set the window title
         self.pack(fill=BOTH, expand=1)
-        quitButton = Button(self, text='Quit', command=self.app_exit)
-        quitButton.place(x=0, y=0)
+        
+#        quitButton = Button(self, text='Quit', command=self.app_exit)
+#        quitButton.place(x=0, y=0)
+        
+        myMenu = Menu(self.master) # Define a menu for the main window
+        self.master.config(menu=myMenu)
+        
+        fileMenu = Menu(myMenu) # Define a menu for the main window menu
+        fileMenu.add_command(label='Exit', command=self.app_exit) # Add an Exit command to the File menu
+        myMenu.add_cascade(label='File', menu=fileMenu) # Add the File menu to the main menu
+        
         
     def app_exit(self):
         self.master.destroy()
         
 mainWindow = Tk()
-mainWindow.geometry('400x300')
+mainWindow.geometry('800x600')
 app = Window(mainWindow)
 mainWindow.mainloop()
