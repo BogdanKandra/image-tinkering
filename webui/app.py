94d6e644f85e37784f2bc67811ddd2700cb1adc8
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-01-24 16:36:00 +0200

    Added placeholder route

--- webui/app.py
+++ webui/app.py
@@ -20,5 +20,12 @@ def testing():
 def operation(name):
     return render_template('test.html', numele=name)
 
+@app.route('/redchannel', methods=['POST'])
+def redchannel():
+    """ The user makes a POST request, sending the name of the file(s) to be
+    operated on. The proper function is applied, obtaining a new image, saving
+    it in the temp zone, and sending the path of the image to the client
+    """
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
