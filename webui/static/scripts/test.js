4a64405d52236f4694a60be6a337e683e86256fd
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-02-16 22:52:16 +0200

    Added function for making POST request for uploading files

--- webui/static/scripts/test.js
+++ webui/static/scripts/test.js
@@ -1,9 +1,50 @@
+// Attach handlers on document ready
 $(document).ready(function() {
 	console.log('In script')
+
 	let imgElement = $('#imagesrc')
-	let inputElement = $('#fileInput')
-	$(inputElement).change(function(event) {
+	let fileInput = $('#fileInput')
+	let uploadInput = $('#uploadInput')
+
+	$(fileInput).change(function(event) {
 		console.log('File was loaded')
-		$(imgElement).attr('src', URL.createObjectURL(event.target.files[0]))
+		if (event.target.files.length != 0) {
+			$(imgElement).attr('src', URL.createObjectURL(event.target.files[0]))
+			$(uploadInput).attr('disabled', false)
+		} else {
+			$(uploadInput).attr('disabled', true)
+		}
 	})
 })
+
+// Makes a POST request, uploading the selected files to the server
+// Upon completion of the upload, the server sends the URLs of the files and the function displays
+// those files
+function uploadFiles() {
+	
+	const files = $('#fileInput').prop('files')
+	console.log('>>>', files)
+	let imageData = new FormData();
+
+	for (let i = 0; i < files.length; i++)
+		imageData.append('files-' + (i + 1), files[i]);
+
+	for (let p of imageData) {
+	  console.log('>>>>>', p);
+	}
+	
+	$.ajax({
+		url: '/uploads',
+		method: 'POST',
+		data: imageData,
+		processData: false,
+		contentType: false,
+		success: function(data) {
+			console.log('Succeeded')
+			console.log('>>> Data', data)
+		},
+		error: function() {
+			console.log('Errored')
+		}
+	})
+}
