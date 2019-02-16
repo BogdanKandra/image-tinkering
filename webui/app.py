fad8704ace2fb2e63b81c4901409de6a81af5fb2
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-02-16 22:43:25 +0200

    Added route for uploads

--- webui/app.py
+++ webui/app.py
@@ -5,8 +5,13 @@ Created on Wed Jan 23 21:20:15 2019
 """
 from flask.app import Flask
 from flask.templating import render_template
+from flask import request
+from werkzeug import secure_filename
+import sys
 
+UPLOAD_FOLDER = '/uploads'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -19,6 +24,16 @@ def testing():
 @app.route('/operation/<name>')
 def operation(name):
     return render_template('test.html', numele=name)
+
+@app.route('/uploads', methods=['POST'])
+def uploads():
+#    print(request, file=sys.stdout)
+#    print('This is standard output', file=sys.stdout)
+    files = request.files
+    print(files, file=sys.stdout)
+    for file in files:
+        file.save(secure_filename(file.filename))
+    return 'HAHAHA'
 
 @app.route('/redchannel', methods=['POST'])
 def redchannel():
