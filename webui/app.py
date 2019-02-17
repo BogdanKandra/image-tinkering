db3a9f9632f98eb624b0c27ded9d259edad9c026
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-02-17 13:22:47 +0200

    Added paths management and finished basic file uploading

--- webui/app.py
+++ webui/app.py
@@ -7,12 +7,18 @@ from flask.app import Flask
 from flask.templating import render_template
 from flask import request
 from werkzeug import secure_filename
-import sys
+import sys, os
 
-UPLOAD_FOLDER = '/uploads'
+# Paths management
+APP_ROOT = os.path.dirname(os.path.abspath(__name__))
+IMAGES_UPLOAD_DIRECTORY = 'static\\uploads\\images'
+VIDEO_UPLOAD_DIRECTORY = 'static\\uploads\\videos'
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['UPLOADED_FILES_DEST'] = UPLOAD_DIRECTORY
+#app.config['UPLOAD_FOLDER'] = UPLOAD_DIRECTORY
 
+# Binding routes
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -28,11 +34,12 @@ def operation(name):
 @app.route('/uploads', methods=['POST'])
 def uploads():
 #    print(request, file=sys.stdout)
-#    print('This is standard output', file=sys.stdout)
-    files = request.files
-    print(files, file=sys.stdout)
-    for file in files:
-        file.save(secure_filename(file.filename))
+    filesDict = request.files
+
+    for key in filesDict:
+        file = filesDict[key]
+        uploadPath = os.path.join(APP_ROOT, IMAGES_UPLOAD_DIRECTORY, secure_filename(file.filename))
+        file.save(uploadPath)
     return 'HAHAHA'
 
 @app.route('/redchannel', methods=['POST'])
@@ -42,5 +49,6 @@ def redchannel():
     it in the temp zone, and sending the path of the image to the client
     """
 
+# Run the application
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
