831bb1dcfe8ddb9e2c95acdff6db360d2fec6c95
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-02-17 13:23:19 +0200

    Removed comments and semicolons

--- webui/static/scripts/test.js
+++ webui/static/scripts/test.js
@@ -1,13 +1,11 @@
 // Attach handlers on document ready
 $(document).ready(function() {
-	console.log('In script')
 
 	let imgElement = $('#imagesrc')
 	let fileInput = $('#fileInput')
 	let uploadInput = $('#uploadInput')
 
 	$(fileInput).change(function(event) {
-		console.log('File was loaded')
 		if (event.target.files.length != 0) {
 			$(imgElement).attr('src', URL.createObjectURL(event.target.files[0]))
 			$(uploadInput).attr('disabled', false)
@@ -23,16 +21,11 @@ $(document).ready(function() {
 function uploadFiles() {
 	
 	const files = $('#fileInput').prop('files')
-	console.log('>>>', files)
-	let imageData = new FormData();
+	let imageData = new FormData()
 
 	for (let i = 0; i < files.length; i++)
-		imageData.append('files-' + (i + 1), files[i]);
+		imageData.append('files-' + (i + 1), files[i])
 
-	for (let p of imageData) {
-	  console.log('>>>>>', p);
-	}
-	
 	$.ajax({
 		url: '/uploads',
 		method: 'POST',
