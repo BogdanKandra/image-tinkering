218efb0894313d800f48b1a8139f805e4c2690d9
unknown <bogdan.kandra@gmail.com>
2019-03-17 20:49:23 +0200

    Detecting whether image is color or grayscale

--- backend/initialisations.py
+++ backend/initialisations.py
@@ -4,23 +4,21 @@ Created on Sun Mar 10 15:12:37 2019
 @author: Bogdan
 """
 import cv2
-import numpy as np
+
 
 def compute_RGB(image):
-    pass
-    
+    """ This function takes an image as parameter and returns a list containing
+    its R, G, B channels or the image itself, if it is grayscale
+    """
+    if len(image.shape) == 2: # Image is grayscale
+        return [image]
+    else:                     # Image is color
+        return [cv2.split(image)]
+
+
 impath = '../webui/static/testinputs/flag.jpg'
-impath2 = '../webui/static/testinputs/gray.png'
-im = cv2.imread(impath, cv2.CV_LOAD_IMAGE_ANYDEPTH)
+impath2 = '../webui/static/testinputs/elaine.tiff'
+impath3 = '../webui/static/testinputs/brontosaur.jpg'
+im = cv2.imread(impath, cv2.IMREAD_UNCHANGED)
 
-#B, G, R = cv2.split(im)
-#if (B == G).all() and (G == R).all():
-#    print('Gray')
-#else:
-#    print('Color')
-
-
-
-#sh = len(im.shape)
-#print(im.shape)
-#compute_RGB(im)
+ims = compute_RGB(im)
