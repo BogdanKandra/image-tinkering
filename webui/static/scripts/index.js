074550f2830af183e7f44c3df7f23cc940a670d2
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-04-14 17:44:23 +0300

    Added many things

--- webui/static/scripts/index.js
+++ webui/static/scripts/index.js
@@ -4,47 +4,56 @@ let imageCapture // Reference used for capturing images
 
 // Attach handlers on document ready
 $(document).ready(function() {
+
 	let fileInput = $('#fileInput')
 	let uploadButton = $('#uploadInput')
 
 	// Display relevant information depending on the number of selected files
-	$(fileInput).change(function(event) {
+	fileInput.change(function(event) {
+
 		let countParagraph = $('#filesCount')
 		let fileNames = $('#fileNames')
+		let filesSource = $('#filesSource')
 		let filesContainer = $('#filesContainer')
+		let steps = $('#steps')
 		let filesCount = event.target.files.length
 
 		// Remove existing file names and images, if user reselects files
-		$(fileNames).empty()
-		$(filesContainer).empty()
+		fileNames.empty()
+		filesContainer.empty()
+		filesSource.text('Disk')
 
 		if (filesCount != 0) {
-			$(uploadButton).removeClass('disabled')
-			$(countParagraph).html('Files Loaded: ' + filesCount) // TODO - Style this to be civilised
+			uploadButton.removeClass('disabled')
+			countParagraph.html('Files Loaded: ' + filesCount) // TODO - Style this to be civilised
 
 			for (let i = 0; i < filesCount; i++) {
 				let fileName = $('<span>').text(event.target.files[i]['name'] + ' ') // TODO - Change to tags
-				$(fileNames).append(fileName)
+				fileNames.append(fileName)
 
 				let imageElement = $('<img>')
-				$(imageElement).prop('src', URL.createObjectURL(event.target.files[i]))
-				$(imageElement).prop('alt', 'Image Preview not Available')
-				$(imageElement).prop('height', '200')
+				imageElement.prop('src', URL.createObjectURL(event.target.files[i]))
+				imageElement.prop('alt', 'Image Preview not Available')
+				imageElement.prop('height', '200')
 
-				$(filesContainer).append($('<div>').append(imageElement))
+				filesContainer.append($('<div>').append(imageElement))
 			}
+
+			steps.css('position', 'absolute')
 		} else {
-			$(uploadButton).addClass('disabled')
-			$(countParagraph).html('No Files loaded yet')
+			uploadButton.addClass('disabled')
+			countParagraph.html('No Files loaded yet')
+			steps.css('position', 'fixed')
 		}
 	})
 })
 
 // Initialises and opens the image capture modal
 function openSelfieModal() {
+
 	$('#selfieModal').modal({
 							onHide: function() {
-								if (imageCapture.track.readyState != 'ended') {
+								if (imageCapture !== undefined && imageCapture.track.readyState != 'ended') {
 									imageCapture.track.stop()
 								}
 							},
@@ -67,6 +76,7 @@ function openSelfieModal() {
 
 // Captures an image from the camera feed
 function captureImage() {
+
 	let snapshot = $('#snapshot')
 	let cameraFeed = $('#cameraFeed')
 	let actionButtons = $('.actions').first().find('.ui')
@@ -96,34 +106,65 @@ function captureImage() {
 
 // Confirms the currently captured image for uploading
 function submitImage() {
+
+	let countParagraph = $('#filesCount')
+	let fileNames = $('#fileNames')
+	let filesSource = $('#filesSource')
 	let filesContainer = $('#filesContainer')
 	let snapshot = $('#snapshot')
 	let uploadButton = $('#uploadInput')
-	let imageElement = $('<img>')
 
+	let imageElement = $('<img>')
 	imageElement.prop('src', snapshot.prop('src'))
 	imageElement.prop('alt', 'Image Preview not Available')
 	imageElement.prop('height', '200')
+
+	countParagraph.html('Files Loaded: 1')
+	fileNames.empty()
+	fileNames.append('Selfie')
+	filesSource.text('Snapshot')
+	filesContainer.empty()
 	filesContainer.append($('<div>').append(imageElement))
 	uploadButton.removeClass('disabled')
 }
 
-// Makes a POST request, uploading the selected files to the server
-// Upon completion of the upload, the server sends the URLs of the files and the function displays
-// those files
+// Builds the request data and sends it to the AJAX call
 function uploadFiles() {
 	// Disable the upload button, while the upload is being performed
 	let uploadButton = $('#uploadInput')
-	$(uploadButton).addClass('disabled')
+	uploadButton.addClass('disabled')
 
 	// Build the request data - a list of selected files
-	const files = $('#fileInput').prop('files')
 	let imageData = new FormData()
-	for (let i = 0; i < files.length; i++) {
-		imageData.append('files-' + (i + 1), files[i])
-	}
+	let filesSource = $('#filesSource')
 
-	// Perform the actual server call which saves the files
+	if (filesSource.text() == 'Disk') {
+		// File source is the file input
+		const files = $('#fileInput').prop('files')
+		for (let i = 0; i < files.length; i++) {
+			imageData.append('files-' + (i + 1), files[i])
+		}
+		uploadAjax(imageData)
+	} else {
+		// File source is the webcam feed
+		let snapshotImg = $('#snapshot')[0]
+		let canvas = document.createElement("canvas")
+		let context = canvas.getContext("2d")
+		canvas.width = snapshotImg.naturalWidth
+		canvas.height = snapshotImg.naturalHeight
+		context.drawImage(snapshotImg, 0, 0)
+		canvas.toBlob(function(blob) {
+			imageData.append('files-1', blob, 'Selfie.jpg')
+			uploadAjax(imageData)
+		})
+	}
+}
+
+// Actually performs the AJAX call which saves the files and initialises its channel and FFT images
+function uploadAjax(imageData) {
+
+	let uploadButton = $('#uploadButton')
+
 	$.ajax({
 		url: '/uploads',
 		method: 'POST',
@@ -141,8 +182,8 @@ function uploadFiles() {
 			
 			console.log('>>>>> Data:', data)
 			
-			// Enable the upload button back
-			$(uploadButton).removeClass('disabled')
+			// Re-enable the upload button
+			uploadButton.removeClass('disabled')
 			
 			// Launch the initialisations procedure
 			$.ajax({
@@ -168,24 +209,24 @@ function uploadFiles() {
 			}).show()
 			
 			// Enable the upload button back
-			$(uploadButton).removeClass('disabled')
+			uploadButton.removeClass('disabled')
 		}
 	})
 }
 
-// This function switches the application mode between 'Image' and 'Video'
+// Switches the application mode between 'Image' and 'Video'
 // TODO - Change the page content accordingly (<title>, <h1> title and other stuff)
 function changeMode() {
 
 	let visibleContent = $('#switchMode').find('.visible').first()
 	let hiddenContentIcon = $('#switchMode').find('.hidden').first().find('.icon').first()
 
-	if ($(visibleContent).html().includes('VIDEO')) {
-		$(visibleContent).html('SWITCH TO IMAGE MODE')
+	if (visibleContent.html().includes('VIDEO')) {
+		visibleContent.html('SWITCH TO IMAGE MODE')
 	} else {
-		$(visibleContent).html('SWITCH TO VIDEO MODE')
+		visibleContent.html('SWITCH TO VIDEO MODE')
 	}
 
-	$(hiddenContentIcon).toggleClass('image')
-	$(hiddenContentIcon).toggleClass('video')
+	hiddenContentIcon.toggleClass('image')
+	hiddenContentIcon.toggleClass('video')
 }
