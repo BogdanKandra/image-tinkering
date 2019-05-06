4c72779e8ea4b085fd1a31f67fa32638323851ad
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-05-06 18:22:42 +0300

    Commented FFT serialisation code

--- webui/modules/initialisations/controllers.py
+++ webui/modules/initialisations/controllers.py
@@ -39,34 +39,34 @@ def initialise():
         if ops.isGrayscale(imageFile):
             # Compute the padded image
             imageH, imageW = imageFile.shape[:2]
-            paddedH, paddedW = 2 * imageH, 2 * imageW
-            paddedImage = np.zeros((paddedH, paddedW), np.uint8)
-            paddedImage[0:imageH, 0:imageW] = imageFile
-            
-            # Compute the FFT of the padded image
-            paddedImageFFT = np.fft.fftshift(np.fft.fft2(paddedImage))
-            
-            # Serialise the FFT of the padded image
-            pickleName = imageNameComponents[0] + '_fft.pickle'
-            picklePath = os.path.join(app.config['TEMP_DATA'], pickleName)
-            file = open(picklePath, 'wb')
-            pickle.dump(paddedImageFFT, file)
-            file.close()
+#            paddedH, paddedW = 2 * imageH, 2 * imageW
+#            paddedImage = np.zeros((paddedH, paddedW), np.uint8)
+#            paddedImage[0:imageH, 0:imageW] = imageFile
+#            
+#            # Compute the FFT of the padded image
+#            paddedImageFFT = np.fft.fftshift(np.fft.fft2(paddedImage))
+#            
+#            # Serialise the FFT of the padded image
+#            pickleName = imageNameComponents[0] + '_fft.pickle'
+#            picklePath = os.path.join(app.config['TEMP_DATA'], pickleName)
+#            file = open(picklePath, 'wb')
+#            pickle.dump(paddedImageFFT, file)
+#            file.close()
         else:
             # Compute the R, G, B channels of the image
             imageChannels = ops.getChannels(imageFile)
             
-            # Compute the padded image
-            imageH, imageW, channels = imageFile.shape
-            paddedH, paddedW = 2 * imageH, 2 * imageW
-            paddedImage = np.zeros((paddedH, paddedW, channels), np.uint8)
-            paddedImage[0:imageH, 0:imageW] = imageFile
-            
-            # Compute the R, G, B channels of the padded image
-            paddedImageChannels = ops.getChannels(paddedImage)
-            
-            # Compute the FFT of the padded image channels
-            paddedImageFFTs = [np.fft.fftshift(np.fft.fft2(channel)) for channel in paddedImageChannels]
+#            # Compute the padded image
+#            imageH, imageW, channels = imageFile.shape
+#            paddedH, paddedW = 2 * imageH, 2 * imageW
+#            paddedImage = np.zeros((paddedH, paddedW, channels), np.uint8)
+#            paddedImage[0:imageH, 0:imageW] = imageFile
+#            
+#            # Compute the R, G, B channels of the padded image
+#            paddedImageChannels = ops.getChannels(paddedImage)
+#            
+#            # Compute the FFT of the padded image channels
+#            paddedImageFFTs = [np.fft.fftshift(np.fft.fft2(channel)) for channel in paddedImageChannels]
             
             # Serialise the image channels
             i = 0
@@ -78,14 +78,14 @@ def initialise():
                 file.close()
                 i += 1
             
-            # Serialise the FFTs of the padded image channels
-            i = 0
-            for c in 'bgr':
-                pickleName = imageNameComponents[0] + '_' + c + '_fft.pickle'
-                picklePath = os.path.join(app.config['TEMP_DATA'], pickleName)
-                file = open(picklePath, 'wb')
-                pickle.dump(paddedImageFFTs[i], file)
-                file.close()
-                i += 1
+#            # Serialise the FFTs of the padded image channels
+#            i = 0
+#            for c in 'bgr':
+#                pickleName = imageNameComponents[0] + '_' + c + '_fft.pickle'
+#                picklePath = os.path.join(app.config['TEMP_DATA'], pickleName)
+#                file = open(picklePath, 'wb')
+#                pickle.dump(paddedImageFFTs[i], file)
+#                file.close()
+#                i += 1
     
     return make_response(jsonify('Initialisations finished!'), 200)
