461136c2e86c9a6c0408ef5b3ea9e4566bc62827
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-05-14 13:41:16 +0300

    Updated main to use dictionary of params

--- backend/main.py
+++ backend/main.py
@@ -17,21 +17,33 @@ if imageH < imageW:
 else:
     cutoff = 0.2 * imageW
 
+paramsGaussian = {
+        'cutoff': cutoff,
+        'type': 'gaussian'
+        }
+
+paramsButterworth = {
+        'cutoff': cutoff,
+        'offset': 1,
+        'type': 'butterworth',
+        'filename': 'bars_15557613543664565.png'
+        }
+
 # Filter the image
-lowFilteredImageIdeal = f.low_pass(image, cutoff, 'ideal')
-lowFilteredImageButter = f.low_pass(image, cutoff, 'butterworth')
-lowFilteredImageGauss = f.low_pass(image, cutoff, 'gaussian')
+#lowFilteredImageIdeal = f.low_pass(image, cutoff, 'ideal')
+#lowFilteredImageButter = f.low_pass(image, cutoff, 'butterworth')
+lowFilteredImageGauss = f.low_pass(image, paramsGaussian)
 #highFilteredImageIdeal = f.high_pass(image, cutoff, 1, 1, 'ideal', filename='bars_15557613543664565.png')
-#highFilteredImageButter = f.high_pass(image, cutoff, 1, 1, 'butterworth', filename='bars_15557613543664565.png')
+highFilteredImageButter = f.high_pass(image, paramsButterworth)
 #highFilteredImageGauss = f.high_pass(image, cutoff, 1, 1, 'gaussian', filename='bars_15557613543664565.png')
 
 # Display results
-cv2.imshow('Original Image', image)
-cv2.imshow('Ideal Low Pass Filtered Image', lowFilteredImageIdeal)
-cv2.imshow('Butterworth Low Pass Filtered Image', lowFilteredImageButter)
+#cv2.imshow('Original Image', image)
+#cv2.imshow('Ideal Low Pass Filtered Image', lowFilteredImageIdeal)
+#cv2.imshow('Butterworth Low Pass Filtered Image', lowFilteredImageButter)
 cv2.imshow('Gaussian Low Pass Filtered Image', lowFilteredImageGauss)
 #cv2.imshow('Ideal High Pass Filtered Image', highFilteredImageIdeal)
-#cv2.imshow('Butterworth High Pass Filtered Image', highFilteredImageButter)
+cv2.imshow('Butterworth High Pass Filtered Image', highFilteredImageButter)
 #cv2.imshow('Gaussian High Pass Filtered Image', highFilteredImageGauss)
 
 cv2.waitKey()
