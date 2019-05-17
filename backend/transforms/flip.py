4be17aa0dc411c38cd9b4a44cbc312d97be4b46f
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-05-17 19:03:16 +0300

    Implemented flip operation

--- backend/transforms/flip.py
+++ backend/transforms/flip.py
@@ -3,5 +3,38 @@ Created on Fri May 17 17:39:42 2019
 
 @author: Bogdan
 """
-def horizontal_flip():
-    NotImplemented
+import numpy as np
+
+
+def flip(image, parameters):
+    """Flips an image along the horizontal or vertical axis.
+    
+    Arguments:
+        *image* (NumPy array) -- the image to flip
+        
+        *parameters* (dictionary) -- a dictionary containing following keys:
+            
+            *axis* (str, optional) -- the axis along which to flip the image;
+            possible values are *horizontal* and *vertical*; default value is 
+            *horizontal*
+    
+    Returns:
+        NumPy array uint8 -- the flipped image
+    """
+    
+    if 'axis' in parameters:
+        axis = parameters['axis']
+    else:
+        axis = 'horizontal'
+    
+    resultImage = np.zeros(image.shape, dtype=np.uint8)
+    if axis == 'horizontal':
+        for i in range(image.shape[0]):
+            resultImage[i] = image[image.shape[0] - i - 1]
+        #resultImage = np.flipud(image)
+    else:
+        for j in range(image.shape[1]):
+            resultImage[:,j] = image[:,image.shape[1] - j - 1]
+        #resultImage = np.fliplr(image)
+    
+    return resultImage
