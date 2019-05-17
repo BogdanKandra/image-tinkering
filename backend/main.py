de5dc668e372d4426109c3e543fa899b06d95730
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-05-17 19:03:41 +0300

    Removed stuff

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,11 @@ import basic_operations as ops
 import time
 
 # Read the input image
-#imagePath = '../webui/static/testinputs/brontosaur.jpg'
-imagePath = '../webui/static/testinputs/bars.png'
-#imagePath = '../webui/static/testinputs/flag.jpg'
+imagePath = '../webui/static/testinputs/brontosaur.jpg' # Grayscale
+#imagePath = '../webui/static/testinputs/chocolate.png' # Alpha channel
+#imagePath = '../webui/static/testinputs/lena.tiff' # TIFF format
+#imagePath = '../webui/static/testinputs/wide.jpg' # 4K image
+
 image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
 
 # Automatically compute the cutoff frequency
@@ -30,21 +32,13 @@ paramsButterworth = {
         }
 
 # Filter the image
-#lowFilteredImageIdeal = f.low_pass(image, cutoff, 'ideal')
-#lowFilteredImageButter = f.low_pass(image, cutoff, 'butterworth')
-lowFilteredImageGauss = f.low_pass(image, paramsGaussian)
-#highFilteredImageIdeal = f.high_pass(image, cutoff, 1, 1, 'ideal', filename='bars_15557613543664565.png')
-highFilteredImageButter = f.high_pass(image, paramsButterworth)
-#highFilteredImageGauss = f.high_pass(image, cutoff, 1, 1, 'gaussian', filename='bars_15557613543664565.png')
+#lowFilteredImageGauss = f.low_pass(image, paramsGaussian)
+#highFilteredImageButter = f.high_pass(image, paramsButterworth)
 
 # Display results
-#cv2.imshow('Original Image', image)
-#cv2.imshow('Ideal Low Pass Filtered Image', lowFilteredImageIdeal)
-#cv2.imshow('Butterworth Low Pass Filtered Image', lowFilteredImageButter)
-cv2.imshow('Gaussian Low Pass Filtered Image', lowFilteredImageGauss)
-#cv2.imshow('Ideal High Pass Filtered Image', highFilteredImageIdeal)
-cv2.imshow('Butterworth High Pass Filtered Image', highFilteredImageButter)
-#cv2.imshow('Gaussian High Pass Filtered Image', highFilteredImageGauss)
+cv2.imshow('Original Image', image)
+#cv2.imshow('Gaussian Low Pass Filtered Image', lowFilteredImageGauss)
+#cv2.imshow('Butterworth High Pass Filtered Image', highFilteredImageButter)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
