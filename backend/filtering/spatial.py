533bcab5780b575cf4ee1af22136d46f87b968b3
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-05-18 18:11:04 +0300

    Implemented negative filtering

--- backend/filtering/spatial.py
+++ backend/filtering/spatial.py
@@ -24,9 +24,24 @@ def grayscale(image, parameters={}):
         NumPy array uint8 -- the filtered image
     """
     if utils.isGrayscale(image):
-        gray = image
+        gray_image = image
     else:
-        gray = image[:,:,0] * 0.11 + image[:,:,1] * 0.59 + image[:,:,2] * 0.3
-        gray = np.uint8(np.rint(gray))
+        gray_image = image[:,:,0] * 0.11 + image[:,:,1] * 0.59 + image[:,:,2] * 0.3
+        gray_image = np.uint8(np.rint(gray_image))
     
-    return gray
+    return gray_image
+
+def negative(image, parameters={}):
+    """Applies a **Negative Filter** on an image. \n
+    
+    Arguments:
+        *image* (NumPy array) -- the image to be filtered
+        
+        *parameters* (dictionary) -- an empty parameter dictionary
+        
+    Returns:
+        NumPy array uint8 -- the filtered image
+    """
+    negative_image = 255 - image
+    
+    return negative_image
