e634b22a0adc0c236221f1ab92b3c0d2db5f1d32
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-05-18 17:33:15 +0300

    Updated utils.py references

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,22 @@
 import cv2
 import numpy as np
-import basic_operations as ops
+import utils
+import filtering.spatial as f
 import time
 
+
 # Read the input image
-imagePath = '../webui/static/testinputs/brontosaur.jpg' # Grayscale
-#imagePath = '../webui/static/testinputs/chocolate.png' # Alpha channel
+#imagePath = '../webui/static/testinputs/brontosaur.jpg' # Grayscale
+imagePath = '../webui/static/testinputs/chocolate.png' # Alpha channel
 #imagePath = '../webui/static/testinputs/lena.tiff' # TIFF format
 #imagePath = '../webui/static/testinputs/wide.jpg' # 4K image
 
 image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
+gray = f.grayscale(image)
 
 # Display results
 cv2.imshow('Original Image', image)
+cv2.imshow('Gray', gray)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
