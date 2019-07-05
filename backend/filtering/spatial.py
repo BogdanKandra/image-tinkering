09c10ebe34e1dd6e8a8e128d774727ad21e25068
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-07-05 11:57:20 +0300

    Added negative and ascii art filters

--- backend/filtering/spatial.py
+++ backend/filtering/spatial.py
@@ -13,6 +13,21 @@ sys.path.append(projectPath)
 from backend import utils
 
 
+def negative(image, parameters={}):
+    """Applies a **Negative Filter** on an image. \n
+    
+    Arguments:
+        *image* (NumPy array) -- the image to be filtered
+        
+        *parameters* (dictionary) -- an empty parameter dictionary
+        
+    Returns:
+        NumPy array uint8 -- the filtered image
+    """
+    negative_image = 255 - image
+    
+    return negative_image
+
 def grayscale(image, parameters={}):
     """Applies a **Grayscale Filter** on an image. \n
     
@@ -67,17 +82,68 @@ def sepia(image, parameters={}):
     
     return sepia
 
-def negative(image, parameters={}):
-    """Applies a **Negative Filter** on an image. \n
+def ascii_art(image, parameters={}):
+    """Applies an **ASCII Art Filter** on an image. \n
     
     Arguments:
         *image* (NumPy array) -- the image to be filtered
         
-        *parameters* (dictionary) -- an empty parameter dictionary
-        
+        *parameters* (dictionary) -- a dictionary containing following keys:
+            
+            *TBD*
+            
     Returns:
         NumPy array uint8 -- the filtered image
     """
-    negative_image = 255 - image
+    # Vars and params
+    #CHARS = ['.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
+    #CHARS = [' ', ',', '.', ':', '-', '=', '+', '*', '#', '%', '@']
+    CHARS = [' ', ',', '.', ':', '-', '=', '+', '*', '%', '@', '#']
+    CHARS = CHARS[::-1] # Reverse the list
+    buckets = 25
     
-    return negative_image
+    def numberToChar(number):
+        return CHARS[number // buckets]
+    
+    # Vectorizing this function allows it to be applied on arrays
+    numberToChar = np.vectorize(numberToChar)
+    
+    # Resize and convert the image to grayscale
+    h, w = image.shape[:2]
+    original_size = (w, h)
+    image = utils.resize(image)
+    if utils.isColor(image):
+        image = grayscale(image)
+    
+    # Build results as list of lines of text and entire text
+    lines = [''.join(numberToChar(row)) for row in list(image)]
+    textSpaceless = ''.join(lines)
+    
+    # Determine the widest letter, to account for the rectangular aspect ratio of the characters
+    font_face = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 1
+    thickness = 1
+    size, base_line = cv2.getTextSize('.', font_face, font_scale, thickness)
+    maximum_letter_width = size[0]
+    
+    for i in range(len(textSpaceless)):
+        letter_width = cv2.getTextSize(textSpaceless[i], font_face, font_scale, thickness)[0][0]
+        if letter_width > maximum_letter_width:
+            maximum_letter_width = letter_width
+    
+    # Create resulting image as white and write text on it
+    numberOfLines = len(lines)
+    numberOfCols = len(lines[0]) * maximum_letter_width
+    dy = 14 # Vertical offset to account for the characters height
+    ascii_image = np.zeros((numberOfLines * dy, numberOfCols), np.uint8)
+    ascii_image[:,:] = 255
+
+    for i, line in enumerate(lines):
+        y = i * dy
+        for j, char in enumerate(line):
+            cv2.putText(ascii_image, char, (j * maximum_letter_width, y), font_face, 1, (0,0,0), 1, lineType=cv2.FILLED)
+    
+    # Display resulting image
+    ascii_image = cv2.resize(ascii_image, original_size, interpolation=cv2.INTER_AREA)
+
+    return ascii_image
