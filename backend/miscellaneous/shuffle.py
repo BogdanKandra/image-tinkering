f353746286ed2fdb69b63109a0a385dc7ac94f51
Bogdan Kandra <bogdan.kandra@gmail.com>
2019-08-03 13:49:16 +0300

    Added miscellaneous package

---
+++ backend/miscellaneous/shuffle.py
@@ -0,0 +1,19 @@
+"""
+Created on Sat Aug  3 13:45:13 2019
+
+@author: Bogdan
+"""
+import numpy as np
+
+
+def pixel_shuffle():
+    pass
+
+def lines_shuffle():
+    pass
+
+def columns_shuffle():
+    pass
+
+def channels_shuffle():
+    pass
