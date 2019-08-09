3bb0e2de02940fcec47811d3e284555ea7a3de97
BogdanKandra <bogdan.kandra@gmail.com>
2019-08-09 18:34:45 +0300

    Implemented channel removal operation

--- backend/miscellaneous/channels.py
+++ backend/miscellaneous/channels.py
@@ -3,6 +3,7 @@ Created on Fri Aug  9 15:26:45 2019
 
 @author: Bogdan
 """
+import copy
 import os, sys
 import cv2
 import numpy as np
@@ -13,7 +14,7 @@ sys.path.append(projectPath)
 from backend import utils
 
 
-def splitChannels(image, parameters):
+def split_channels(image, parameters):
     """ Splits an image into its channels and returns them.
     
     Arguments:
@@ -48,7 +49,42 @@ def splitChannels(image, parameters):
     else:
         return [image]
 
-def removeChannel(image, parameters):
+def remove_channels(image, parameters):
+    """ Zeroes out channels from an image.
+    
+    Arguments:
+        *image* (NumPy array) -- the image from which to remove channels
+        
+        *parameters* (dictionary) -- a dictionary containing following keys:
+            
+            *channel(s)* (str) -- the channel(s) to be removed from the image;
+            possible values are *red*, *green*, *blue*, *red & green*, *red & 
+            blue*, *green & blue*
+    Returns:
+        Numpy array uint8 -- the image having the requested channels removed
     """
-    """
-    pass
+    channelsInformation = parameters['channel(s)']
+    image_copy = copy.deepcopy(image)
+    
+    if '&' in channelsInformation:
+        # Zero out the two specified channels
+        channels = channelsInformation.split(' & ')
+        if channels[0] == 'red':
+            image_copy[:,:,2] = 0
+        elif channels[0] == 'green':
+            image_copy[:,:,1] = 0
+        
+        if channels[1] == 'green':
+            image_copy[:,:,1] = 0
+        elif channels[1] == 'blue':
+            image_copy[:,:,0] = 0
+    else:
+        # Zero out the specified channel
+        if channelsInformation == 'red':
+            image_copy[:,:,2] = 0
+        elif channelsInformation == 'green':
+            image_copy[:,:,1] = 0
+        else:
+            image_copy[:,:,0] = 0
+    
+    return image_copy
