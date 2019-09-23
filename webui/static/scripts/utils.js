14fda72ba9595642d07f9d6a6a5ee1f09d90a881
BogdanKandra <bogdan.kandra@gmail.com>
2019-09-23 17:54:28 +0300

    Moved AJAX calls to utils

--- webui/static/scripts/utils.js
+++ webui/static/scripts/utils.js
@@ -31,3 +31,63 @@ function changeMode() {
 	hiddenContentIcon.toggleClass('image')
 	hiddenContentIcon.toggleClass('video')
 }
+
+// Performs an AJAX call which deletes the resulting images (processed images and pickles) for each of the given list of processed files
+function deleteTempdataAjax(fileNamesList) {
+
+	$.ajax({
+        url: '/cleanup/tempdata',
+        method: 'POST',
+        data: JSON.stringify({'filenames': fileNamesList}),
+        contentType: 'application/json',
+        success: function(_data) {},
+        error: function(_request, _status, _error) {
+			console.log('>>> An error occured during deletion of processed files and pickles')
+        }
+    })
+}
+
+// Performs an AJAX call which deletes the pickle files which resulted after the initialisation process, for each of the given list of processed files
+function deletePicklesAjax(fileNamesList) {
+
+    $.ajax({
+        url: '/cleanup/pickles',
+        method: 'POST',
+        data: JSON.stringify({'filenames': fileNamesList}),
+        contentType: 'application/json',
+        success: function(_data) {},
+        error: function(_request, _status, _error) {
+            console.log('>>> An error occured during deletion of pickles')
+        }
+    })
+}
+
+// Performs an AJAX call which deletes uploaded images (input image and extra input images, if any), for each of the given keys in the data to process or list of filenames
+function deleteUploadsAjax(dataToProcessObject) {
+
+	$.ajax({
+		url: '/cleanup/uploads',
+		method: 'POST',
+		data: JSON.stringify({'data': dataToProcessObject}),
+		contentType: 'application/json',
+		success: function(_data) {},
+		error: function(_request, _status, _error) {
+			console.log('>>> An error occured during deletion of uploads')
+		}
+	})
+}
+
+// Performs an AJAX call which deletes any existent extra input images associated to the image to be configured
+function deleteExtraInputsAjax(imageName) {
+
+    $.ajax({
+        url: '/cleanup/extras',
+        method: 'POST',
+        data: JSON.stringify({'name': imageName}),
+        contentType: 'application/json',
+        success: function(_data) {},
+        error: function(_request, _status, _error) {
+            console.log('>>> An error occured during deletion of extra input images')
+        }
+    })
+}
