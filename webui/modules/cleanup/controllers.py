747d75a95254f64c58cad85efb6e9f48426ad032
BogdanKandra <bogdan.kandra@gmail.com>
2019-09-23 17:59:07 +0300

    Added new route and refactored

--- webui/modules/cleanup/controllers.py
+++ webui/modules/cleanup/controllers.py
@@ -13,9 +13,10 @@ from flask.json import jsonify
 
 CLEANUP_MOD = Blueprint('cleanup', __name__, url_prefix='/cleanup')
 
-@CLEANUP_MOD.route('/results', methods=['POST'])
-def cleanup_results():
-    ''' Deletes the resulting images (processed images and pickles) '''
+@CLEANUP_MOD.route('/tempdata', methods=['POST'])
+def cleanup_tempdata():
+    ''' Deletes the resulting images (processed images and pickles)
+    for each of the given list of processed files '''
     filenames = request.get_json()['filenames']
 
     for filename in filenames:
@@ -25,9 +26,9 @@ def cleanup_results():
             if os.path.exists(processed_path):
                 os.remove(processed_path)
             else:
-                print('>>> [/cleanup/results] Requested file does not exist:', filename)
+                print('>>> [/cleanup/tempdata] Requested file does not exist:', filename)
         except OSError as err:
-            print('>>> [/cleanup/results] Error deleting file:', filename)
+            print('>>> [/cleanup/tempdata] Error deleting file:', filename)
             print('>>>', err)
 
         # Delete the pickled channels
@@ -39,16 +40,40 @@ def cleanup_results():
                 if os.path.exists(pickle_path):
                     os.remove(pickle_path)
                 else:
-                    print('>>> [/cleanup/results] Pickle does not exist:', pickle_name)
+                    print('>>> [/cleanup/tempdata] Pickle does not exist:', pickle_name)
             except OSError as err:
-                print('>>> [/cleanup/results] Error deleting pickle:', pickle_name)
+                print('>>> [/cleanup/tempdata] Error deleting pickle:', pickle_name)
                 print('>>>', err)
 
     return make_response(jsonify('Server: Result files have been deleted'), 200)
 
+@CLEANUP_MOD.route('/pickles', methods=['POST'])
+def cleanup_pickles():
+    ''' Deletes the pickle files which resulted after the initialisation process,
+    for each of the given keys in the data to process or list of filenames to process '''
+    filenames = request.get_json()['filenames']
+
+    for filename in filenames:
+        # Delete the pickled channels
+        unprocessed_filename = filename.rsplit('.')[0]
+        for channel in 'rgba':
+            pickle_name = unprocessed_filename + '_' + channel + '.pickle'
+            pickle_path = os.path.join(app.config['TEMP_DATA'], pickle_name)
+            try:
+                if os.path.exists(pickle_path):
+                    os.remove(pickle_path)
+                else:
+                    print('>>> [/cleanup/pickles] Pickle does not exist:', pickle_name)
+            except OSError as err:
+                print('>>> [/cleanup/pickles] Error deleting pickle:', pickle_name)
+                print('>>>', err)
+
+    return make_response(jsonify('Server: Pickle files have been deleted'), 200)
+
 @CLEANUP_MOD.route('/uploads', methods=['POST'])
 def cleanup_uploads():
-    ''' Deletes uploaded images (and extra images, if necessary) '''
+    ''' Deletes uploaded images (input image and extra input images, if any),
+    for each of the given keys in the data to process or list of filenames to process '''
     data = request.get_json()['data']
 
     for file_name in data:
@@ -78,7 +103,7 @@ def cleanup_uploads():
 
 @CLEANUP_MOD.route('/extras', methods=['POST'])
 def cleanup_extras():
-    ''' Deletes extra images associated to a particular input image '''
+    ''' Deletes extra input images associated to a particular input image '''
     image_name = request.get_json()['name']
     extra_inputs = [file for file in os.listdir(app.config['EXTRA_IMAGES_DIR'])
                     if file.startswith(image_name.split('.')[0] + '_')]
