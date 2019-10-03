72eab0522fd4d3543e5c9948c9e8faa0dfcc45a8
BogdanKandra <bogdan.kandra@gmail.com>
2019-10-03 19:58:04 +0300

    Added imports

--- backend/watermarking/spatial.py
+++ backend/watermarking/spatial.py
@@ -5,11 +5,12 @@ Created on Thu Oct  3 19:50:10 2019
 """
 import os
 import sys
+import utils as wmutils
 project_path = os.getcwd()
 while os.path.basename(project_path) != 'image-tinkering':
     project_path = os.path.dirname(project_path)
 sys.path.append(project_path)
-from backend import utils
+from backend import utils as projutils
 
 
 def test():
