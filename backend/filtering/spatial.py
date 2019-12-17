fe19eeb7d1daccaab1310c10c0e3bb59a2d25453
BogdanKandra <bogdan.kandra@gmail.com>
2019-12-17 10:40:55 +0200

    Implemented Canny Edge Algorithm

--- backend/filtering/spatial.py
+++ backend/filtering/spatial.py
@@ -360,10 +360,13 @@ def edge(image, extra_inputs, parameters):
         *parameters* (dictionary) -- a dictionary containing following keys:
 
             *method* (str) -- the type of edge detection algorithm to be applied; possible values
-            are *canny* and *laplacian*
+            are *laplacian*, *gradient*, *non-max suppression*, *canny*
 
-            *pre-blur* (str, optional) -- whether to apply blur to the input image before convolving
-            with the laplacian kernel; possible values are 'Yes' and 'No'; default value is 'Yes'
+            *pre-Blur* (str, optional) -- whether to apply blur to the input image before convolving
+            with the laplacian kernel; possible values are 'yes' and 'no'; default value is 'yes'
+
+            *edge_Count* (str, optional) -- how many edges wil be considered strong by the Canny
+            Edge Algorithm; possible values are *standard* and *many*; default value is *standard*
 
     Returns:
         list of NumPy array uint8 -- list containing the filtered image
@@ -371,19 +374,21 @@ def edge(image, extra_inputs, parameters):
     # Parameters extraction
     method = parameters['method']
 
-    if 'pre-blur' in parameters:
-        pre_blur = parameters['pre-blur']
-        if pre_blur == 'Yes':
+    if 'pre-Blur' in parameters:
+        pre_blur = parameters['pre-Blur']
+        if pre_blur == 'yes':
             pre_blur = True
         else:
             pre_blur = False
     else:
         pre_blur = True
 
-    # Perform Edge Detection
-    if method == 'canny':       # This method assumes applying the Canny Edge Detection algorithm
-        pass
-    elif method == 'laplacian': # This method assumes convolving the image with the Laplacian kernel
+    if 'edge_Count' in parameters:
+        count = parameters['edge_Count']
+    else:
+        count = 'standard'
+
+    if method == 'laplacian':  # The Laplacian operator of the image is computed
         # Preprocess the input image
         if pre_blur:
             gray = grayscale(image, {}, {})[0]
@@ -392,7 +397,7 @@ def edge(image, extra_inputs, parameters):
         else:
             processed_image = grayscale(image, {}, {})[0]
 
-        # Convolve processed image with laplacian kernel
+        # Convolve processed image with the laplacian kernel
         laplacian_kernel_alt = np.array([[-1, -1, -1],
                                          [-1, 8, -1],
                                          [-1, -1, -1]])
@@ -401,6 +406,129 @@ def edge(image, extra_inputs, parameters):
 #                                           [-1, 4, -1],
 #                                           [0, -1, 0]])
 
-        edges_image = helpers.apply_kernel(processed_image, laplacian_kernel_alt)
+        laplacian_image = helpers.apply_kernel(processed_image, laplacian_kernel_alt)
 
-    return [edges_image]
+        return [laplacian_image]
+    else:  # The Canny Edge Algorithm is implemented
+        ### Preprocess the input image -- blurring is necessary so noise is removed
+        gray = grayscale(image, {}, {})[0]
+        blur_kernel = helpers.generate_gaussian_kernel(3, 1)
+        blurred = helpers.apply_kernel(gray, blur_kernel)
+
+        ### Image Gradient computation (Angles and Normalized Magnitudes)
+        sobel_x_left = np.array([[-1, 0, 1],
+                                 [-2, 0, 2],
+                                 [-1, 0, 1]])
+        sobel_y_bottom = np.array([[1, 2, 1],
+                                   [0, 0, 0],
+                                   [-1, -2, -1]])
+        sobel_x_right = np.array([[1, 0, -1],
+                                  [2, 0, -2],
+                                  [1, 0, -1]])
+        sobel_y_top = np.array([[-1, -2, -1],
+                                [0, 0, 0],
+                                [1, 2, 1]])
+
+        derivative_x_left = helpers.apply_kernel(blurred, sobel_x_left)
+        derivative_x_right = helpers.apply_kernel(blurred, sobel_x_right)
+        derivative_x = derivative_x_left + derivative_x_right
+
+        derivative_y_bottom = helpers.apply_kernel(blurred, sobel_y_bottom)
+        derivative_y_top = helpers.apply_kernel(blurred, sobel_y_top)
+        derivative_y = derivative_y_bottom + derivative_y_top
+
+        gradient_magnitude = np.hypot(derivative_x, derivative_y)
+        gradient_magnitude = gradient_magnitude / gradient_magnitude.max() * 255
+
+        if method == 'gradient':
+            return [gradient_magnitude.astype(np.uint8)]
+
+        ### Non-Maximum Suppression step
+        height, width = image.shape[:2]
+
+        gradient_radians = np.arctan2(derivative_y, derivative_x)
+        gradient_angles = np.degrees(gradient_radians)  # radians * 180 / pi
+        gradient_angles[gradient_angles < 0] += 180
+
+        # Put all angles in 4 bins, corresponding to their approximate direction
+        gradient_angles = np.where((((0 <= gradient_angles) & (gradient_angles < 22.5)) |
+                                   ((157.5 <= gradient_angles) & (gradient_angles <= 180))), 0, gradient_angles)
+        gradient_angles = np.where((22.5 <= gradient_angles) & (gradient_angles < 67.5), 45, gradient_angles)
+        gradient_angles = np.where((67.5 <= gradient_angles) & (gradient_angles < 112.5), 90, gradient_angles)
+        gradient_angles = np.where((112.5 <= gradient_angles) & (gradient_angles < 157.5), 135, gradient_angles)
+
+        suppressed = gradient_magnitude.copy()
+        # Zero out the border of the image, since they can't contain any edges
+        suppressed[0, :] = suppressed[-1, :] = suppressed[:, 0] = suppressed[:, -1] = 0
+
+        for px in range(1, height - 1):
+            for py in range(1, width - 1):
+                current_magnitude = gradient_magnitude[px, py]
+                current_angle = gradient_angles[px, py]
+
+                # Determine the pixels on the same direction as the current pixel
+                if current_angle == 0:     # 0' and 180' directions
+                    same_direction_pixel_1 = gradient_magnitude[px, py + 1]
+                    same_direction_pixel_2 = gradient_magnitude[px, py - 1]
+                elif current_angle == 45:  # 45' direction
+                    same_direction_pixel_1 = gradient_magnitude[px + 1, py - 1]
+                    same_direction_pixel_2 = gradient_magnitude[px - 1, py + 1]
+                elif current_angle == 90:  # 90' direction
+                    same_direction_pixel_1 = gradient_magnitude[px + 1, py]
+                    same_direction_pixel_2 = gradient_magnitude[px - 1, py]
+                elif current_angle == 135: # 135' direction
+                    same_direction_pixel_1 = gradient_magnitude[px - 1, py - 1]
+                    same_direction_pixel_2 = gradient_magnitude[px + 1, py + 1]
+
+                # Suppress the current pixel if not maximum among neighbours on same direction
+                if current_magnitude < same_direction_pixel_1 or current_magnitude < same_direction_pixel_2:
+                    suppressed[px, py] = 0
+
+        if method == 'non-max suppression':
+            return [suppressed.astype(np.uint8)]
+
+        ### Hysteresis-Thresholding step
+        # Thresholds are determined automatically, based on percentiles from the gradient magnitudes
+        sigma = 0.33
+        if count == 'standard':
+            percentile = 75
+        elif count == 'many':
+            percentile = 50
+
+        while True:
+            percentile_value = np.percentile(gradient_magnitude, percentile)
+            if percentile_value == 0:
+                percentile += 1
+            else:
+                break
+
+        lower_threshold = int(max(0, percentile_value * (1 - sigma * 1.5)))
+        upper_threshold = int(min(255, percentile_value * (1 + sigma)))
+
+        weak_intensity = 25
+        strong_intensity = 255
+
+        # Double Thresholding is performed -- pixels having gradient intensity higher than
+        # upper_threshold are considered strong edges, those with an intensity lower than
+        # lower_threshold are considered weak edges and zeroed out and the rest are labeled as weak
+        thresholded = np.where(suppressed >= upper_threshold, strong_intensity, suppressed)
+        thresholded = np.where(thresholded < lower_threshold, 0, thresholded)
+        thresholded = np.where((thresholded >= lower_threshold) &
+                                (thresholded < upper_threshold), weak_intensity, thresholded)
+
+        # Hysteresis -- weak edge pixels are labeled as strong if they have at least one strong
+        # surrounding pixel
+        mask_x, mask_y = np.where(thresholded == weak_intensity)
+        mask = np.array(list(zip(mask_x, mask_y)))
+
+        for [x, y] in mask:
+            try:
+                pixel_neighbourhood_maximum = thresholded[x - 1 : x + 2, y - 1 : y + 2].max()
+                if pixel_neighbourhood_maximum == strong_intensity:
+                    thresholded[x, y] = strong_intensity
+                else:
+                    thresholded[x, y] = 0
+            except IndexError:
+                pass
+
+        return [thresholded.astype(np.uint8)]
