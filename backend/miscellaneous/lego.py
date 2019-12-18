d6ae6423947fe7c6bbb11d8fcf8d73579aafd20e
BogdanKandra <bogdan.kandra@gmail.com>
2019-12-18 12:28:55 +0200

    Moved ASCII art operation from spatial filtering to misc and implemented Sketchify operation

--- backend/miscellaneous/lego.py
+++ backend/miscellaneous/lego.py
@@ -93,6 +93,94 @@ def build_mosaic(image, technique, alpha_level, resolution, redundancy):
 
     return mosaic_image
 
+def ascii_art(image, extra_inputs, parameters):
+    '''Applies an **ASCII Art Filter** onto an image. \n
+
+    Arguments:
+        *image* (NumPy array) -- the image to be filtered
+
+        *extra_inputs* (dictionary) -- a dictionary holding any extra inputs for the call (empty)
+
+        *parameters* (dictionary) -- a dictionary containing following keys:
+
+            *charset* (str, optional) -- the character set to use when rendering
+            ASCII art image; possible values are *standard*, *alternate* and *full*
+
+    Returns:
+        list of NumPy array uint8 -- list containing the filtered image
+    '''
+    # Small, 11 character ramps
+    STANDARD_CHARSET = [' ', '.', ',', ':', '-', '=', '+', '*', '#', '%', '@']  # "Standard"
+    ALTERNATE_CHARSET = [' ', '.', ',', ':', '-', '=', '+', '*', '%', '@', '#']   # "Alternate"
+
+    # Full, 70 character ramp
+    FULL_CHARSET = [' ', '.', '\'', '`', '^', '"', ',', ':', ';', 'I', 'l', '!',
+                    'i', '>', '<', '~', '+', '_', '-', '?', ']', '[', '}', '{',
+                    '1', ')', '(', '|', '\\', '/', 't', 'f', 'j', 'r', 'x', 'n',
+                    'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0',
+                    'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o',
+                    '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$']
+
+    if 'charset' in parameters:
+        if parameters['charset'] == 'standard':
+            CHARS = STANDARD_CHARSET
+        elif parameters['charset'] == 'alternate':
+            CHARS = ALTERNATE_CHARSET
+        else:
+            CHARS = FULL_CHARSET
+    else:
+        CHARS = ALTERNATE_CHARSET
+
+    buckets = 256 / len(CHARS)
+    CHARS = CHARS[::-1] # Reverse the list
+
+    def number_to_char(number):
+        return CHARS[int(number // buckets)]
+
+    # Vectorizing this function allows it to be applied on arrays
+    number_to_char = np.vectorize(number_to_char)
+
+    # Resize and convert the image to grayscale
+    h, w = image.shape[:2]
+    original_size = (w, h)
+    image = utils.resize_dimension(image, new_width=80)
+    if utils.is_color(image):
+        image = grayscale(image)[0]
+
+    # Build results as list of lines of text and entire text
+    lines = [''.join(number_to_char(row)) for row in list(image)]
+    text_spaceless = ''.join(lines)
+
+    # Determine the widest letter, to account for the rectangular aspect ratio of the characters
+    font_face = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 1
+    thickness = 1
+    size, base_line = cv2.getTextSize('.', font_face, font_scale, thickness)
+    maximum_letter_width = size[0]
+
+    for i in range(len(text_spaceless)):
+        letter_width = cv2.getTextSize(text_spaceless[i], font_face, font_scale, thickness)[0][0]
+        if letter_width > maximum_letter_width:
+            maximum_letter_width = letter_width
+
+    # Create resulting image as white and write text on it
+    number_of_lines = len(lines)
+    number_of_cols = len(lines[0]) * maximum_letter_width
+    dy = 14 # Vertical offset to account for the characters height
+    ascii_image = np.zeros((number_of_lines * dy, number_of_cols), np.uint8)
+    ascii_image[:, :] = 255
+
+    for i, line in enumerate(lines):
+        y = i * dy
+        for j, char in enumerate(line):
+            cv2.putText(ascii_image, char, (j * maximum_letter_width, y), font_face, 1, \
+                        (0, 0, 0), 1, lineType=cv2.FILLED)
+
+    # Resize resulting image to original size of input image
+    ascii_image = cv2.resize(ascii_image, original_size, interpolation=cv2.INTER_AREA)
+
+    return [ascii_image]
+
 def photomosaic(image, extra_inputs, parameters):
     ''' Builds a photomosaic which approximates the given image, using the database image tiles.
 
