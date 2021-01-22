31039f4e6568bb0ada431c157a18f9b746d5755e
BogdanKandra <bogdan.kandra@gmail.com>
2021-01-22 20:34:29 +0200

    Slightly improved implementation

--- webui/modules/initialisations/controllers.py
+++ webui/modules/initialisations/controllers.py
@@ -39,59 +39,48 @@ def initialise():
         if utils.is_grayscale(image_file):
             # Compute the padded image
             image_h, image_w = image_file.shape[:2]
-#            padded_h, padded_w = 2 * image_h, 2 * image_w
-#            padded_image = np.zeros((padded_h, padded_w), np.uint8)
-#            padded_image[0:image_h, 0:image_w] = image_file
+            # padded_h, padded_w = 2 * image_h, 2 * image_w
+            # padded_image = np.zeros((padded_h, padded_w), np.uint8)
+            # padded_image[0:image_h, 0:image_w] = image_file
 
-#            # Compute the FFT of the padded image
-#            padded_image_FFT = np.fft.fftshift(np.fft.fft2(padded_image))
+            # # Compute the FFT of the padded image
+            # padded_image_FFT = np.fft.fftshift(np.fft.fft2(padded_image))
 
-#            # Serialise the FFT of the padded image
-#            pickle_name = image_name_components[0] + '_fft.pickle'
-#            pickle_path = os.path.join(app.config['TEMP_DATA'], pickle_name)
-#            file = open(pickle_path, 'wb')
-#            pickle.dump(padded_image_FFT, file)
-#            file.close()
+            # # Serialise the FFT of the padded image
+            # pickle_name = image_name_components[0] + '_fft.pickle'
+            # pickle_path = os.path.join(app.config['TEMP_DATA'], pickle_name)
+            # with open(pickle_path, 'wb') as f:
+            #     pickle.dump(padded_image_FFT, f)
         else:
             # Compute the R, G, B channels of the image
             image_channels = utils.get_channels(image_file)
 
             # Serialise the image channels
-            i = 0
+            channels_string = 'bgra'
 
-            if len(image_channels) == 4:
-                channels_string = 'bgra'
-            else:
-                channels_string = 'bgr'
-
-            for c in channels_string:
-                pickle_name = image_name_components[0] + '_' + c + '.pickle'
+            for i in range(len(image_channels)):
+                pickle_name = image_name_components[0] + '_' + channels_string[i] + '.pickle'
                 pickle_path = os.path.join(app.config['TEMP_DATA'], pickle_name)
-                file = open(pickle_path, 'wb')
-                pickle.dump(image_channels[i], file)
-                file.close()
-                i += 1
+                with open(pickle_path, 'wb') as f:
+                    pickle.dump(image_channels[i], f)
 
-#            # Compute the padded image
-#            image_h, image_w, channels = image_file.shape
-#            padded_h, padded_w = 2 * image_h, 2 * image_w
-#            padded_image = np.zeros((padded_h, padded_w, channels), np.uint8)
-#            padded_image[0:image_h, 0:image_w] = image_file
+            # # Compute the padded image
+            # image_h, image_w, channels = image_file.shape
+            # padded_h, padded_w = 2 * image_h, 2 * image_w
+            # padded_image = np.zeros((padded_h, padded_w, channels), np.uint8)
+            # padded_image[0:image_h, 0:image_w] = image_file
 
-#            # Compute the R, G, B channels of the padded image
-#            padded_image_channels = utils.get_channels(padded_image)
+            # # Compute the R, G, B channels of the padded image
+            # padded_image_channels = utils.get_channels(padded_image)
 
-#            # Compute the FFT of the padded image channels
-#            padded_image_FFTs = [np.fft.fftshift(np.fft.fft2(channel)) for channel in padded_image_channels]
+            # # Compute the FFT of the padded image channels
+            # padded_image_FFTs = [np.fft.fftshift(np.fft.fft2(channel)) for channel in padded_image_channels]
 
-#            # Serialise the FFTs of the padded image channels
-#            i = 0
-#            for c in 'bgr':
-#                pickle_name = image_name_components[0] + '_' + c + '_fft.pickle'
-#                pickle_path = os.path.join(app.config['TEMP_DATA'], pickle_name)
-#                file = open(pickle_path, 'wb')
-#                pickle.dump(padded_image_FFTs[i], file)
-#                file.close()
-#                i += 1
+            # # Serialise the FFTs of the padded image channels
+            # for i in range(len(padded_image_FFTs)):
+            #     pickle_name = image_name_components[0] + '_' + channels_string[i] + '_fft.pickle'
+            #     pickle_path = os.path.join(app.config['TEMP_DATA'], pickle_name)
+            #     with open(pickle_path, 'wb') as f:
+            #         pickle.dump(padded_image_FFTs[i], f)
 
     return make_response(jsonify('Server: Initialisations process completed successfully'), 200)
